dac4a9eccca3b41d876da90c386a31719469bd4c
Kin Hong NG <kinhong99@gmail.com>
2026-07-10 13:47:12 -0400

    terraform(jet): add nfs server

--- infra/terraform/jet/kubernetes/compute/vms.tf
+++ infra/terraform/jet/kubernetes/compute/vms.tf
@@ -1,4 +1,16 @@
 ################
+#NFS Volume
+################
+
+resource "openstack_blockstorage_volume_v3" "nfs_data" {
+  name = "nfs-data"
+  # In GB
+  size        = 500
+  volume_type = "__DEFAULT__"
+  description = "Persistent storage for Kubernetes NFS"
+}
+
+################
 #VMs
 ################
 
@@ -44,6 +56,37 @@ resource "openstack_compute_instance_v2" "ubuntu24_sm" {
   }
 }
 
+resource "openstack_compute_instance_v2" "nfs" {
+  name            = "kubernetes-nfs-server"
+  image_id        = var.image_id
+  flavor_id       = 2
+  key_pair        = var.os_public_key
+  security_groups = ["nfs-server"]
+  power_state     = var.node_power_state
+
+  block_device {
+    uuid                  = var.image_id
+    source_type           = "image"
+    destination_type      = "local"
+    boot_index            = 0
+    delete_on_termination = true
+  }
+
+  block_device {
+    uuid                  = openstack_blockstorage_volume_v3.nfs_data.id
+    source_type           = "volume"
+    destination_type      = "volume"
+    boot_index            = 1
+    delete_on_termination = false
+  }
+
+  network {
+    name = var.private_network
+  }
+
+  user_data = file("${path.module}/templates/cloud-init.yaml")
+}
+
 ################
 #FIPS 
 ################
@@ -56,6 +99,10 @@ resource "openstack_networking_floatingip_v2" "masters" {
 resource "openstack_networking_floatingip_v2" "workers" {
   count = var.worker_node_count
   pool  = "public"
+}
+
+resource "openstack_networking_floatingip_v2" "nfs" {
+  pool = "public"
 }
 
 ################
@@ -76,6 +123,11 @@ data "openstack_networking_port_v2" "worker_ports" {
   network_id = openstack_compute_instance_v2.ubuntu24_sm[count.index].network[0].uuid
 }
 
+data "openstack_networking_port_v2" "nfs_ports" {
+  device_id  = openstack_compute_instance_v2.nfs.id
+  network_id = openstack_compute_instance_v2.nfs.network[0].uuid
+}
+
 ################
 #FIPS Association
 ################
@@ -92,4 +144,9 @@ resource "openstack_networking_floatingip_associate_v2" "workers" {
 
   floating_ip = openstack_networking_floatingip_v2.workers[count.index].address
   port_id     = data.openstack_networking_port_v2.worker_ports[count.index].id
+}
+
+resource "openstack_networking_floatingip_associate_v2" "nfs" {
+  floating_ip = openstack_networking_floatingip_v2.nfs.address
+  port_id     = data.openstack_networking_port_v2.nfs_ports.id
 }
